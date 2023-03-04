04ec7597c20a92ad03468728da05b67d6ccaa140
rezziqbal <iqbal@cenah.co.id>
2023-03-04 10:54:50 +0700

    debug

--- app/controller/api_front/asesmen.php
+++ app/controller/api_front/asesmen.php
@@ -626,6 +626,8 @@ class Asesmen extends JI_Controller
 	{
 		$d = $this->__init();
 		$data = array();
+		$this->_api_auth_required($data, 'user');
+
 		$this->status = 200;
 		$this->message = API_ADMIN_ERROR_CODES[$this->status];
 
--- app/core/ji_controller.php
+++ app/core/ji_controller.php
@@ -692,6 +692,7 @@ class JI_Controller extends \SENE_Controller
     protected function _api_auth_required($data = null, $pov = 'admin', $utype = 'session')
     {
         $is_login = $this->{$pov . '_login'};
+        dd($is_login);
         // if ($pov == 'user') $is_login = $this->user_login ?? $this->reseller_login ?? 0;
         if ($utype == 'session' && !$is_login) {
             $this->status = 400;
